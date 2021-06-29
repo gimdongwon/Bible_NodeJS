5873552c400d0e549a45fbef95ec32a1fe566f7a
gimdongwon <ehddnjs8989@gmail.com>
2021-06-29 19:23:57 +0900

    nosql mongodb 학습완료

---
+++ chapter8/public/mongoose.js
@@ -0,0 +1,147 @@
+// 사용자 이름을 클릭할 때 댓글로딩
+
+document.querySelectorAll("#user-list tr").forEach((el) => {
+  el.addEventListener("click", function () {
+    const id = el.querySelector("td").textContent;
+    document.querySelector("#userid").value = id;
+    getComment(id);
+  });
+});
+
+// 사용자 로딩
+
+async function getUser() {
+  try {
+    const res = await axios.get("/users");
+    const users = res.data;
+    console.log(users);
+    const tbody = document.querySelector("#user-list tbody");
+    tbody.innerHTML = "";
+    users.map(function (user) {
+      const row = document.createElement("tr");
+      row.addEventListener("click", () => {
+        getComment(user._id);
+      });
+      let td = document.createElement("td");
+      td.textContent = user._id;
+      row.appendChild(td);
+      td = document.createElement("td");
+      td.textContent = user.name;
+      row.appendChild(td);
+      td = document.createElement("td");
+      td.textContent = user.age;
+      row.appendChild(td);
+      td = document.createElement("td");
+      td.textContent = user.married ? "기혼" : "미혼";
+      row.appendChild(td);
+      tbody.appendChild(row);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// 댓글로딩
+
+async function getComment(id) {
+  try {
+    const res = await axios.get(`/users/${id}/comments`);
+    const comments = res.data;
+    const tbody = document.querySelector("#comment-list tbody");
+    tbody.innerHTML = "";
+    comments.map(function (comment) {
+      const row = document.createElement("tr");
+      let td = document.createElement("td");
+      td.textContent = comment._id;
+      row.appendChild(td);
+      td = document.createElement("td");
+      td.textContent = comment.name;
+      row.appendChild(td);
+      td = document.createElement("td");
+      td.textContent = comment.comment;
+      row.appendChild(td);
+      const edit = document.createElement("button");
+      edit.textContent = "수정";
+      edit.addEventListener("click", async () => {
+        const newComment = prompt("바꿀 내용을 입력하시오");
+        if (!newComment) {
+          return alert("내용을 반드시 입력하시오");
+        }
+        try {
+          await axios.patch(`/comments/${comment._id}`, {
+            comment: newComment,
+          });
+          getComment(id);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+      const remove = document.createElement("button");
+      remove.textContent = "삭제";
+      remove.addEventListener("click", async (e) => {
+        try {
+          await axios.delete(`/comments/${comment._id}`);
+          getComment(id);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+      td = document.createElement("td");
+      td.append(edit);
+      row.appendChild(td);
+      td = document.createElement("td");
+      td.append(remove);
+      row.appendChild(td);
+      tbody.appendChild(row);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// 사용자 등록시
+document.getElementById("user-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = e.target.username.value;
+  const age = e.target.age.value;
+  const married = e.target.married.checked;
+  if (!name) {
+    return alert("이름을 입력하시오");
+  }
+  if (!age) {
+    return alert("나이를 입력하시오");
+  }
+  try {
+    await axios.post("/users", { name, age, married });
+    getUser();
+  } catch (error) {
+    console.error(error);
+  }
+  e.target.username.value = "";
+  e.target.age.value = "";
+  e.target.married.value = false;
+});
+
+// 댓글 등록시
+
+document
+  .getElementById("comment-form")
+  .addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const id = e.target.userid.value;
+    const comment = e.target.comment.value;
+    if (!id) {
+      return alert("아이디를 입력하세요");
+    }
+    if (!comment) {
+      return alert("댓글을 입력하세요");
+    }
+    try {
+      const dd = await axios.post("/comments", { id, comment });
+      getComment(id);
+    } catch (error) {
+      console.error(error);
+    }
+    e.target.userid.value = "";
+    e.target.comment.value = "";
+  });
