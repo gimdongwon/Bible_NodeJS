a3456dd5d6dbf7c31a25134371e882a41f728cc3
gimdongwon <ehddnjs8989@gmail.com>
2021-07-06 18:08:51 +0900

    script 수정으로 open하고 server 동시 실행

--- chapter9/routes/user.js
+++ chapter9/routes/user.js
@@ -22,9 +22,22 @@ router.post("/:id/follow", isLoggedIn, async (req, res, next) => {
 router.post("/:id/unfollow", isLoggedIn, async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { id: req.user.id } });
-    console.log(user, "$$$$$$");
     if (user) {
       await user.removeFollowing(parseInt(req.params.id, 10));
+      res.send("success");
+    } else {
+      res.status(404).send("no user");
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+router.post("/:id/getFollowing", isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      await user.getFollowing(parseInt(req.params.id, 10));
       res.send("success");
     } else {
       res.status(404).send("no user");
